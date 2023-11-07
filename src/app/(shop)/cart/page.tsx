240181b5b207dcb7c481f0c8fe71bbfaee6f276d
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-07 16:01:17 -0500

    Actualizaciones

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -79,7 +79,7 @@ export default function () {
 
 
           {/* Checkout - Resumen de orden */ }
-          <div className="bg-white rounded-xl shadow-xl p-7">
+          <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
             <h2 className="text-2xl mb-2">Resumen de orden</h2>
 
             <div className="grid grid-cols-2">
