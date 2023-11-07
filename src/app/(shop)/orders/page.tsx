514c69a302e5a776069f2c8938aa8e6e9c87e026
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-07 15:03:24 -0500

    Pantalla de Ordenes creada

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -1,8 +1,77 @@
+// https://tailwindcomponents.com/component/hoverable-table
+import { Title } from '@/components';
+
+import Link from 'next/link';
+import { IoCardOutline } from 'react-icons/io5';
 
 export default function() {
   return (
-    <div>
-      <h1>Listado de ordenes</h1>
-    </div>
+    <>
+      <Title title="Orders" />
+
+      <div className="mb-10">
+        <table className="min-w-full">
+          <thead className="bg-gray-200 border-b">
+            <tr>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                #ID
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Nombre completo
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Estado
+              </th>
+              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Opciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Mark
+              </td>
+              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+
+                <IoCardOutline className="text-green-800" />
+                <span className='mx-2 text-green-800'>Pagada</span>
+
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 ">
+                <Link href="/orders/123" className="hover:underline">
+                  Ver orden
+                </Link>
+              </td>
+
+            </tr>
+
+            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                Mark
+              </td>
+              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+
+                <IoCardOutline className="text-red-800" />
+                <span className='mx-2 text-red-800'>No Pagada</span>
+
+              </td>
+              <td className="text-sm text-gray-900 font-light px-6 ">
+                <Link href="/orders/123" className="hover:underline">
+                  Ver orden
+                </Link>
+              </td>
+
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
