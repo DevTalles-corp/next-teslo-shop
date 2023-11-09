0fe57e0287269cec9974faa98514d3605f012016
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-09 16:25:23 -0500

    CategoriesMap creado

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,18 +1,47 @@
 import { initialData } from './seed';
-
+import prisma from '../lib/prisma';
 
 
 
 async function main() {
 
+  // 1. Borrar registros previos
+  await Promise.all( [
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany(),
+  ]);
+  
+  const { categories, products } = initialData;
 
-  console.log(initialData);
+
+  //  Categorias
+  // {
+  //   name: 'Shirt'
+  // }
+  const categoriesData = categories.map( (name) => ({ name }));
+  
+  await prisma.category.createMany({
+    data: categoriesData
+  });
 
   
+  const categoriesDB = await prisma.category.findMany();
+  
+  const categoriesMap = categoriesDB.reduce( (map, category) => {
+    map[ category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>); //<string=shirt, string=categoryID>
+
+  console.log(categoriesMap);
+
+  // Productos
 
 
 
-  console.log('Seed ejecutado correctamente');
+
+
+  console.log( 'Seed ejecutado correctamente' );
 }
 
 
@@ -23,10 +52,10 @@ async function main() {
 
 
 
-(() => {
+( () => {
 
-  if (process.env.NODE_ENV === 'production') return;
+  if ( process.env.NODE_ENV === 'production' ) return;
 
 
   main();
-})();
+} )();
