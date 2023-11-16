eb95886a8ea1d1dc3a721cd998a1735f6a9b1722
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-16 14:12:25 -0500

    Metadata agregada

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,11 +1,17 @@
 export const revalidate = 604800; //7 días
+import { Metadata, ResolvingMetadata } from "next";
 
+import { notFound } from "next/navigation";
 
-import { notFound } from 'next/navigation';
-
-import { titleFont } from '@/config/fonts';
-import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector, StockLabel } from '@/components';
-import { getProductBySlug } from '@/actions';
+import { titleFont } from "@/config/fonts";
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+  StockLabel,
+} from "@/components";
+import { getProductBySlug } from "@/actions";
 
 interface Props {
   params: {
@@ -13,81 +19,85 @@ interface Props {
   };
 }
 
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
 
+  // fetch data
+  const product = await getProductBySlug(slug);
 
-export default async function ProductBySlugPage( { params }: Props ) {
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
 
+  return {
+    title: product?.title ?? "Producto no encontrado",
+    description: product?.description ?? "",
+    openGraph: {
+      title: product?.title ?? "Producto no encontrado",
+      description: product?.description ?? "",
+      // images: [], // https://misitioweb.com/products/image.png
+      images: [ `/products/${ product?.images[1] }`],
+    },
+  };
+}
+
+export default async function ProductBySlugPage({ params }: Props) {
   const { slug } = params;
   const product = await getProductBySlug(slug);
   console.log(product);
 
-  if ( !product ) {
+  if (!product) {
     notFound();
   }
 
-
-
   return (
     <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
-
-      {/* Slideshow */ }
+      {/* Slideshow */}
       <div className="col-span-1 md:col-span-2 ">
-        
         {/* Mobile Slideshow */}
-        <ProductMobileSlideshow 
-          title={ product.title }
-          images={ product.images }
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
           className="block md:hidden"
         />
-        
+
         {/* Desktop Slideshow */}
-        <ProductSlideshow 
-          title={ product.title }
-          images={ product.images }
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
           className="hidden md:block"
         />
-
-        
       </div>
 
-      {/* Detalles */ }
+      {/* Detalles */}
       <div className="col-span-1 px-5">
+        <StockLabel slug={product.slug} />
 
-        
-        <StockLabel slug={ product.slug } />
-
-        <h1 className={ ` ${ titleFont.className } antialiased font-bold text-xl` }>
-          { product.title }
+        <h1 className={` ${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
         </h1>
-        
-        <p className="text-lg mb-5">${ product.price }</p>
 
-        {/* Selector de Tallas */ }
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* Selector de Tallas */}
         <SizeSelector
-          selectedSize={ product.sizes[ 1 ] }
-          availableSizes={ product.sizes }
+          selectedSize={product.sizes[1]}
+          availableSizes={product.sizes}
         />
 
+        {/* Selector de Cantidad */}
+        <QuantitySelector quantity={2} />
 
-        {/* Selector de Cantidad */ }
-        <QuantitySelector 
-          quantity={ 2 }
-        />
+        {/* Button */}
+        <button className="btn-primary my-5">Agregar al carrito</button>
 
-
-        {/* Button */ }
-        <button className="btn-primary my-5">
-          Agregar al carrito
-        </button>
-
-        {/* Descripción */ }
+        {/* Descripción */}
         <h3 className="font-bold text-sm">Descripción</h3>
-        <p className="font-light">
-          { product.description }
-        </p>
-
+        <p className="font-light">{product.description}</p>
       </div>
-
     </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,10 @@ import './globals.css';
 
 
 export const metadata: Metadata = {
-  title: 'Teslo | Shop',
+  title: {
+    template: '%s - Teslo | Shop',
+    default: 'Home - Teslo | Shop'
+  },
   description: 'Una tienda virtual de productos',
 }
 
