3d800e9f59ac474873174c6fad255cc9a5fea1e0
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-21 10:51:06 -0500

    Seed creado

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -8,12 +8,20 @@ async function main() {
 
   // 1. Borrar registros previos
   // await Promise.all( [
+  await prisma.user.deleteMany();
+
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
   // ]);
   
-  const { categories, products } = initialData;
+  const { categories, products, users } = initialData;
+
+
+  await prisma.user.createMany({
+    data: users
+  });
+
 
 
   //  Categorias
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,3 +1,6 @@
+import bcryptjs from 'bcryptjs';
+
+
 interface SeedProduct {
   description: string;
   images: string[];
@@ -11,10 +14,20 @@ interface SeedProduct {
   gender: 'men' | 'women' | 'kid' | 'unisex';
 }
 
+interface SeedUser {
+  email: string;
+  password: string;
+  name: string;
+  role: 'admin'|'user'
+}
+
+
+
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
 interface SeedData {
+  users: SeedUser[];
   categories: string[];
   products: SeedProduct[];
 }
@@ -23,6 +36,24 @@ interface SeedData {
 
 
 export const initialData: SeedData = {
+
+  users: [
+    {
+      email: 'fernando@google.com',
+      name: 'Fernando Herrera',
+      password: bcryptjs.hashSync('123456'),
+      role: 'admin'
+    },
+    {
+      email: 'melissa@google.com',
+      name: 'Melissa Flores',
+      password: bcryptjs.hashSync('123456'),
+      role: 'user'
+    },
+
+
+  ],
+
 
   categories: [
     'Shirts', 'Pants', 'Hoodies', 'Hats'
