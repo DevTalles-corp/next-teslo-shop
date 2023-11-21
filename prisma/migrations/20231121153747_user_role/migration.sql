2e7bbab0d998c584612b6035e69fb722a227673c
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-21 10:41:46 -0500

    Usuario y Role agregado

---
+++ prisma/migrations/20231121153747_user_role/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'user');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
