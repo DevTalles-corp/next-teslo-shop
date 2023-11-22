d30c7d5c082cc9427285e58a5f213ff45b827ac6
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-22 15:26:02 -0500

    Esquema de Prisma - User Address

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -4,6 +4,8 @@ import clsx from 'clsx';
 import { useForm } from 'react-hook-form';
 
 import type { Country } from '@/interfaces';
+import { useAddressStore } from '@/store';
+import { useEffect } from 'react';
 
 
 type FormInputs = {
@@ -27,15 +29,31 @@ interface Props {
 export const AddressForm = ({ countries }: Props) => {
 
   
-  const { handleSubmit, register, formState: { isValid } } = useForm<FormInputs>({
+  const { handleSubmit, register, formState: { isValid }, reset } = useForm<FormInputs>({
     defaultValues: {
       // Todo: leer de la base de datos
     }
   });
 
+  const setAddress = useAddressStore( state => state.setAddress );
+  const address = useAddressStore( state => state.address );
+
+
+  useEffect(() => {
+    if ( address.firstName ) {
+      reset(address)
+    }
+  },[])
+  
+
+
+
 
   const onSubmit = ( data: FormInputs ) => {
     console.log({data});
+
+    setAddress(data);
+
   }
 
 
