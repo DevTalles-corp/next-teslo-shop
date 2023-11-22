b0365c01053bc5730f5246a77c5a08e379a6f0e0
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-22 14:49:25 -0500

    Seed actualizado con países

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,6 +1,6 @@
-import { create } from 'zustand';
-import { initialData } from './seed';
 import prisma from '../lib/prisma';
+import { initialData } from './seed';
+import { countries } from './seed-countries';
 
 
 
@@ -9,6 +9,7 @@ async function main() {
   // 1. Borrar registros previos
   // await Promise.all( [
   await prisma.user.deleteMany();
+  await prisma.country.deleteMany();
 
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
@@ -20,6 +21,10 @@ async function main() {
 
   await prisma.user.createMany({
     data: users
+  });
+
+  await prisma.country.createMany({
+    data: countries
   });
 
 
