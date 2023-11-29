b5da6c39f6555e943cbe792eb5100c18e69b5c3c
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-29 14:02:03 -0500

    Cloudinary añadido

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
